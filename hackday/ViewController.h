51cd01f32f0c35b3b0c86c936cb95b813d9af282
guanbeiqi@gmail.com <guanbeiqi@jiuzhilan.net>
2014-12-06 16:05:12 -0800

    Basic front-end implementation

--- hackday/ViewController.h
+++ hackday/ViewController.h
@@ -7,9 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "question.h"
 @interface ViewController : UIViewController
-
 
 @end
 
--- hackday/ViewController.m
+++ hackday/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "question.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *question;
+@property (weak, nonatomic) IBOutlet UITextField *choice1;
+@property (weak, nonatomic) IBOutlet UITextField *choice2;
+@property(strong,nonatomic) NSMutableArray *myquesions;
 
 @end
 
@@ -17,11 +22,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.myquesions = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)postQuestion:(id)sender {
+    NSString *content = [NSString stringWithFormat:self.question.text];
+    NSString *firstChoice = [NSString stringWithFormat:self.choice1.text];
+    NSString *secondChoice =[ NSString stringWithFormat:self.choice2.text];
+    question *newQuestion = [[question alloc] initWithString: content
+                                                       first: firstChoice second: secondChoice];
+    [self.myquesions addObject:newQuestion];
+    
 }
 
 @end
